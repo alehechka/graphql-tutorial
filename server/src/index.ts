0d32c380cbd4677e3f55a9b022f5321cce1b0e73
alehechka <42357034+alehechka@users.noreply.github.com>
2020-10-02 15:38:47 -0500

    Created first query

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello');
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema';
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+
+app.listen(4000, () => {
+	console.log(`Listening on port 4000...`);
+});
